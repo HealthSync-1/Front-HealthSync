255359a6946f2e9317de31c22b20022d25f1dd3e
MaiconDouglas-dev <maicon.timot8@gmail.com>
2025-09-30 18:36:10 -0300

    Criação do data e informações dos integrantes.

---
+++ imrea-teleconsulta/src/data/integrantes.ts
@@ -0,0 +1,35 @@
+export type Integrante = {
+  rm: string;
+  nome: string;
+  turma: string;
+  img: string;
+  github: string;
+  linkedin: string;
+};
+
+export const INTEGRANTES: Integrante[] = [
+  {
+    rm: "561279",
+    nome: "Maicon Douglas",
+    turma: "1TDSPW",
+    img: "/img/Maicon.jpg",
+    github: "https://github.com/MaiconDouglas-dev",
+    linkedin: "https://www.linkedin.com/in/maicon-douglas-b244571b5/",
+  },
+  {
+    rm: "566127",
+    nome: "Richard Freitas",
+    turma: "1TDSPW",
+    img: "/img/Richard.jpg",
+    github: "https://github.com/vlonerickk",
+    linkedin: "https://linkedin.com/in/richard-freitas",
+  },
+  {
+    rm: "566376",
+    nome: "Laura Lopes",
+    turma: "1TDSPw",
+    img: "/img/Pedro.png",
+    github: "https://github.com/Laura853",
+    linkedin: "https://www.linkedin.com/in/laura-lopes-a5937a353",
+  },
+];
