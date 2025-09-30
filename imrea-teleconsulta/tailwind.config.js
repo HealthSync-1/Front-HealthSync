d4dcd2f8f70eeaa4485def764a37f04231a3eb23
MaiconDouglas-dev <maicon.timot8@gmail.com>
2025-09-30 18:41:14 -0300

    Criação do tailwindConfig e inde.css.

---
+++ imrea-teleconsulta/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        imrea: { green: '#006b54', green2: '#03896c' },
+      },
+    },
+  },
+  plugins: [],
+};
