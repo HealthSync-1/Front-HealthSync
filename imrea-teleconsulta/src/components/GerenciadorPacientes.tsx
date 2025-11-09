06cee73e6927a628fe94556e6a8f7aaf6aedfba3
MaiconDouglas-dev <maicon.timot8@gmail.com>
2025-11-09 18:17:18 -0300

    feat: Criado os componentes React

--- imrea-teleconsulta/src/components/GerenciadorPacientes.tsx
+++ imrea-teleconsulta/src/components/GerenciadorPacientes.tsx
@@ -9,3 +9,18 @@ interface Paciente {
 }
 type PacienteDTO = Omit<Paciente, 'id'>;
 const API_URL = 'https://healthsync-apirestful.onrender.com/pacientes';
+
+ 
+function GerenciadorPacientes() {
+  const [pacientes, setPacientes] = useState<Paciente[]>([]);
+ 
+  const [formData, setFormData] = useState<PacienteDTO>({
+    nome: '',
+    cpf: '',
+    dataNascimento: '',
+  });
+ 
+  const [editingId, setEditingId] = useState<number | null>(null);
+ 
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
